bef911147f3263d74616a7b26828d62a8d62a983
wanglx <1318659588@qq.com>
2022-09-22 21:50:28 +0800

    1

--- banip_ssh.sh
+++ banip_ssh.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# 说明：使用nali过滤白名单地理区域IP或IP地址，fail2ban自动封禁ssh暴力破解IP地址；
+# 
 
 # vars
 logfile=/var/log/banip_ssh.log
 
-white_list="btmp begins|Cogent|154.3.36.145|加利福尼亚州洛杉矶Cogent通信"
+# white strings or ip list, '|' split string
+white_list="btmp begins|Cogent|加利福尼亚州洛杉矶Cogent通信"
 
 banip_list=($(last -f /var/log/btmp  |nali |grep -Ev "${white_list}" |awk '{print $3}' |sort -n |uniq))
 
@@ -15,19 +18,24 @@ banip(){
   sudo fail2ban-client set ssh-iptables banip $@
 }
 
+main(){
+echo -e "\n#### [ $(date +%F' '%T)] show ssh banip list ####"   |tee -a ${logfile}
+
 # ban ip action
 for i in "${banip_list[@]}"
 do
   # echo "banip $i"
   banip $i >/dev/null ;ret=$?
   if [ ! $ret -eq 0 ]; then
-    echo "ACT: banip $i is failed " |tee -a ${logfile}
+    echo "WARN: banip $i is failed " |tee -a ${logfile}
   fi
-  sleep 0.1
+  sleep 0.05
 done
 
 sleep 1
 
 # list ban ip
-echo -e "\n#### [ $(date +%F' '%T)] show ssh banip list ####"   |tee -a ${logfile}
 sudo fail2ban-client status ssh-iptables   |tee -a ${logfile}
+}
+
+main
